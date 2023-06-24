9cfb248c2add973356db2be5f0b00d73d2409096
Gabriel Brocco <gabsbrocco@gmail.com>
2023-06-24 15:52:22 -0300

    Tornando funcional os comandos F10, F11 e F12. E adicionado o try / except no código

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import winsound
 from tkinter import simpledialog
 import sys
+import os
 pygame.init()
 
 tamanho = (800,600)
@@ -26,55 +27,109 @@ salvaPontos = "Pressione F10 para SALVAR os pontos marcados"
 carregaPontos = "Pressione F11 para CARREGAR os pontos marcados anteriormente"
 deletaPontos = "Pressione F12 para DELETAR os pontos"
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            posicao = pygame.mouse.get_pos()
-            estrelas.append(posicao)
 
-            # Perguntar o nome da estrela 
-            nome = simpledialog.askstring("Nome", "Digite um nome para a estrela:")
-            nomesEstrelas.append(nome)
-            print(nomesEstrelas)
+try:
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                posicao = pygame.mouse.get_pos()
+                estrelas.append(posicao)
 
-            # Adicionar a coordenada marcada
-            cordenadasMarcadas.append(posicao)
-            print(cordenadasMarcadas)
+                # Perguntar o nome da estrela 
+                nome = simpledialog.askstring("Nome", "Digite um nome para a estrela:")
+                nomesEstrelas.append(nome)
+                print(nomesEstrelas)
 
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                # Adicionar a coordenada marcada
+                cordenadasMarcadas.append(posicao)
+                print(cordenadasMarcadas)
+
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                posicao = pygame.mouse.get_pos()
+                estrelas.append(posicao)
+                cordenadasMarcadas.append(posicao)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F10:
+                    #Salvar 
+                    with open("estrelas_salvas.txt", "w") as arquivo:
+                        for estrela, nome in zip(estrelas, nomesEstrelas):
+                            arquivo.write(f"{estrela[0]},{estrela[1]},{nome}\n")
+                    print("Estrelas marcadas salvas com sucesso!")
+                    
+                elif event.key == pygame.K_F11:
+                    #Carregar
+                    if os.path.isfile("estrelas_salvas.txt"):
+                        estrelas.clear()
+                        nomesEstrelas.clear()
+                        with open("estrelas_salvas.txt", "r") as arquivo:
+                            linhas = arquivo.readlines()
+                            for linha in linhas:
+                                x, y, nome = linha.strip().split(",")
+                                estrelas.append((int(x), int(y)))
+                                nomesEstrelas.append(nome)
+                        print("Estrelas marcadas carregadas com sucesso!")
+                    else:
+                        print("Nenhum arquivo de estrelas salvas encontrado.")
+                        
+                elif event.key == pygame.K_F12:
+                    #Deletar
+                    if os.path.isfile("estrelas_salvas.txt"):
+                        os.remove("estrelas_salvas.txt")
+                        estrelas.clear()
+                        nomesEstrelas.clear()
+                        print("Estrelas marcadas deletadas com sucesso!")
+                    else:
+                        print("Nenhum arquivo de estrelas salvas encontrado.")
+            #Fechar janela
+            elif event.type == pygame.QUIT: 
+                running = False
+                print("--------------------" "\n" "The game was closed!" "\n" "--------------------")     
+
+        fundo = pygame.image.load("background.jpg")
+        tela.blit(fundo, (0,0))
+        
+        #Mostrar estrelas e nomes
+        for posicao, nome in zip(estrelas, nomesEstrelas):
+            pygame.draw.circle(janela, white, posicao, 5)
+            texto_nome = fonte.render(nome, True, white)
+            texto_coordenadas = fonte.render(f"({posicao[0]}, {posicao[1]})", True, white)
+            tela.blit(texto_nome, (posicao[0], posicao[1] + 10))
+            tela.blit(texto_coordenadas, (posicao[0], posicao[1] + 25))
+        if len(estrelas) >= 2:
+            for i in range(len(estrelas) - 1):
+                pygame.draw.line(tela, white, estrelas[i], estrelas[i+1])
+        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             posicao = pygame.mouse.get_pos()
             estrelas.append(posicao)
             nomesEstrelas.append("desconhecido")
             cordenadasMarcadas.append(posicao)
+        
+        for posicao, nome in zip(estrelas, nomesEstrelas):
+            pygame.draw.circle(janela, white, posicao, 5)
+            texto_nome = fonte.render(nome, True, white)
+            texto_coordenadas = fonte.render(f"({posicao[0]}, {posicao[1]})", True, white)
+            tela.blit(texto_nome, (posicao[0], posicao[1] + 10))
+            tela.blit(texto_coordenadas, (posicao[0], posicao[1] + 25))
+        
+
+        def comandos():
+            texto = fonte.render(salvaPontos,True, white )
+            tela.blit(texto, (10,10))
             
-
-    fundo = pygame.image.load("background.jpg")
-    tela.blit(fundo, (0,0))
-    
-    # Desenhar as estrelas e os nomes
-    for posicao, nome in zip(estrelas, nomesEstrelas):
-        pygame.draw.circle(janela, white, posicao, 5)
-        texto_nome = fonte.render(nome, True, white)
-        texto_coordenadas = fonte.render(f"({posicao[0]}, {posicao[1]})", True, white)
-        tela.blit(texto_nome, (posicao[0], posicao[1] + 10))
-        tela.blit(texto_coordenadas, (posicao[0], posicao[1] + 25))
-    if len(estrelas) >= 2:
-        for i in range(len(estrelas) - 1):
-            pygame.draw.line(tela, white, estrelas[i], estrelas[i+1])
-
-    def comandos():
-        texto = fonte.render(salvaPontos,True, white )
-        tela.blit(texto, (10,10))
+            texto = fonte.render(carregaPontos,True, white )
+            tela.blit(texto, (10,25))
+            
+            texto = fonte.render(deletaPontos,True, white )
+            tela.blit(texto, (10,40))
         
-        texto = fonte.render(carregaPontos,True, white )
-        tela.blit(texto, (10,25))
+        comandos()
         
-        texto = fonte.render(deletaPontos,True, white )
-        tela.blit(texto, (10,40))
-    
-    comandos()
-    
-    
-    pygame.display.update()
+        
+        pygame.display.update()
 
-pygame.quit()
+except Exception as e:
+    print("Ocorreu um erro!", str(e))
+
+finally:
+    pygame.quit()
